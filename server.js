48323b573159fcf2e0b071f2631733a7a392a485
SantiBass <sibarraryan@gmail.com>
2022-01-10 11:34:32 -0800

    working on server.js

--- server.js
+++ server.js
@@ -2,7 +2,36 @@ const path =  require('path');
 const express = require('express');
 const session =  require('express-session');
 const routes =  require('./controllers');
+const helpers =  require('./utils/helpers');
 const sequelize =  require('./config/connection');
+const PORT = process.env.PORT || 3005;
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const exphbs = require('express-handlebars');
+const app = express();
+const hbs = exphbs.create({ helpers });
+const sess = {
+  secret: 'Mountainoffire',
+  cookie: { expires: 15 * 60 * 1000
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+// const =  ();
+// const =  ();
+// const =  ();
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-module.exports = sequelize;
+app.use(routes);
+sequelize.sync({force: false}).then(()=>{
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}! It's online`);
+    });
+    });
